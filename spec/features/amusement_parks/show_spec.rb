9b15a79cd76dce3d161141b954df162df6acca64
Lito White <leland.white@gmail.com>
2020-07-17 09:50:07 -0700

    Completed User Story 2

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe 'As a visitor' do
   describe 'When I visit a parks show page' do
     before :each do
       @amusement_park = AmusementPark.create!(name: "Six Flage", admission_price: 50.00)
-      @ride_1 = Ride.create!(name: "Water Splash", thrill_rating: 8)
-      @ride_2 = Ride.create!(name: "Merry Go Round", thrill_rating: 2)
+      @ride_1 = Ride.create!(name: "Water Splash", thrill_rating: 8, amusement_park_id: @amusement_park.id)
+      @ride_2 = Ride.create!(name: "Merry Go Round", thrill_rating: 2, amusement_park_id: @amusement_park.id)
+
+      visit "/parks/#{@amusement_park.id}"
     end
 
+
     it 'I see the name and price of admission to that park' do
-      visit "/parks/#{@amusement_park.id}"
 
       expect(page).to have_content(@amusement_park.name)
       expect(page).to have_content(@amusement_park.admission_price)
@@ -17,10 +19,13 @@ RSpec.describe 'As a visitor' do
 
     it 'I see the names of all rides at that park in alphabetical order' do
 
+      expect(page).to have_content(@ride_2.name)
+      expect(page).to have_content(@ride_1.name)
+
     end
 
     it 'I see the average thrill rating of the rides ' do
-
+       expect(page).to have_content("Average Thrill Rating of Rides: 5.0/10")
     end
 
   end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AmusementPark do
+
   describe 'relationships' do
     it {should have_many :rides}
+  end
 
+  describe 'methods' do
+    it '#average_thrill_rating' do
+      amusement_park = AmusementPark.create!(name: "Six Flage", admission_price: 50.00)
+      ride_1 = Ride.create!(name: "Water Splash", thrill_rating: 8, amusement_park_id: amusement_park.id)
+      ride_2 = Ride.create!(name: "Merry Go Round", thrill_rating: 2, amusement_park_id: amusement_park.id)
+
+      expect(amusement_park.average_thrill_rating).to eq(5.0)
+    end
   end
 end
