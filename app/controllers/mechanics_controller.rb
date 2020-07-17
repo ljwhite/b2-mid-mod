63d578b4db043dabdc62a9eb107bd71f94269324
Lito White <leland.white@gmail.com>
2020-07-17 10:57:39 -0700

    commit for pr, last user story not complete

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,7 +1,18 @@
 class MechanicsController < ApplicationController
 
   def index
-    @mechanics = Mechanic.all 
+    @mechanics = Mechanic.all
+  end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+  end
+
+  def add
+    mechanic = Mechanic.find(params[:id])
+    ride = Ride.find(params[:ride_id])
+    mechanic.rides << ride
+    redirect_to "mechanics/#{mechanic.id}"
   end
 
 end
---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'When I visit a mechanics show page' do
+
+    before :each do
+      @amusement_park = AmusementPark.create!(name: "Six Flage", admission_price: 50.00)
+
+      @ride_1 = Ride.create!(name: "Water Splash", thrill_rating: 8, amusement_park_id: @amusement_park.id)
+      @ride_2 = Ride.create!(name: "Merry Go Round", thrill_rating: 2, amusement_park_id: @amusement_park.id)
+      @ride_3 = Ride.create!(name: "Roller Coaster", thrill_rating: 10, amusement_park_id: @amusement_park.id)
+
+      @mechanic_1 = Mechanic.create!(name: "Tim", years_experience: 5)
+
+      MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_1.id)
+      MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_2.id)
+
+      visit "/mechanics/#{@mechanic_1.id}"
+    end
+
+    it 'I see their name, years experience, and all rides they are working on' do
+
+      expect(page).to have_content(@mechanic_1.name)
+      expect(page).to have_content(@mechanic_1.years_experience)
+      expect(page).to have_content(@ride_1.name)
+      expect(page).to have_content(@ride_2.name)
+
+    end
+
+    it 'there is also a field to add a ride for them to work on' do
+
+      fill_in :ride_id, with: "#{@ride_3.id}"
+      click_on "Add Ride"
+      expect(page).to have_content(@ride_3.name)
+
+    end
+
+  end
+end
